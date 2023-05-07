0d2b44164cfd1e3f52d07a91951f657101637bc0
Eero Kokkonen <eero.kokkonen@hotmail.com>
2023-05-07 21:33:08 +0300

    as

--- Program.cs
+++ Program.cs
@@ -3,5 +3,16 @@
 namespace Days;
 class Program
 {
-    static void Main(string[] args) => CommandLineApplication.Execute<Days>(args);
+    static void Main(string[] args)
+    {
+        try
+        {
+            DataHandler.GetEvents();
+        }
+        catch (Exception ex)
+        {
+            System.Console.WriteLine("Could not find days.csv, creating a new one...");
+        }
+        CommandLineApplication.Execute<Days>(args);
+    }
 }
